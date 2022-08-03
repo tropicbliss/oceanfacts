d442d2c2ebe4a8019af7d953812e769e7f3f0b06
tropicbliss <tropicbliss@protonmail.com>
2022-08-04 02:15:15 +0800

    E

--- components/Nav.js
+++ components/Nav.js
@@ -42,22 +42,26 @@ const Nav = () => {
         </button>
         <div className="w-full md:block md:w-auto" hidden={!menuExpanded}>
           <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-              <Link onClick={() => setMenuExpanded(false)} href="/about">
-                About
-              </Link>
+            <li
+              onClick={() => setMenuExpanded(false)}
+              className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+            >
+              <Link href="/about">About</Link>
             </li>
-            <li className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-              <Link onClick={() => setMenuExpanded(false)} href="/apidocs">
-                API
-              </Link>
+            <li
+              onClick={() => setMenuExpanded(false)}
+              className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+            >
+              <Link href="/apidocs">API</Link>
             </li>
-            <li className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+            <li
+              onClick={() => setMenuExpanded(false)}
+              className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+            >
               <a
                 href="https://github.com/tropicbliss/oceanfacts"
                 target="_blank"
                 rel="noreferrer"
-                onClick={() => setMenuExpanded(false)}
               >
                 GitHub
               </a>
