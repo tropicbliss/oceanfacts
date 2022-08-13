3a8f343f11a39e784afe5481b99a7d523bf7fbf1
tropicbliss <tropicbliss@protonmail.com>
2022-08-13 15:16:57 +0800

    Add hyperlinks to api

--- components/Nav.js
+++ components/Nav.js
@@ -58,13 +58,9 @@ const Nav = () => {
               onClick={() => setMenuExpanded(false)}
               className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
             >
-              <a
-                href="https://github.com/tropicbliss/oceanfacts"
-                target="_blank"
-                rel="noreferrer"
-              >
+              <Link href="https://github.com/tropicbliss/oceanfacts">
                 GitHub
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
--- pages/apidocs.js
+++ pages/apidocs.js
@@ -1,8 +1,10 @@
+import Link from "next/link";
+
 const apidocs = () => {
   return (
     <div className="p-3 mt-20">
       <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-        GET /api/facts
+        <Link href="/api/facts">GET /api/facts</Link>
       </h2>
       <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
         Description: Returns all the ocean facts.
@@ -14,7 +16,7 @@ const apidocs = () => {
         200: Successfully got ocean facts.
       </p>
       <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-        GET /api/facts/[id]
+        <Link href="/api/facts/0">GET /api/facts/[id]</Link>
       </h2>
       <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
         Description: Returns an ocean fact that corresponds to an id.
@@ -26,7 +28,7 @@ const apidocs = () => {
         200: Successfully got an ocean fact.
       </p>
       <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-        GET /api/facts/random
+        <Link href="/api/facts/random">GET /api/facts/random</Link>
       </h2>
       <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
         Description: Returns a random ocean fact.
