244b6cfd8d4ce8dae1ab7f401587599d270cfbdd
tropicbliss <tropicbliss@protonmail.com>
2022-08-18 19:31:39 +0800

    E

--- pages/fact/[id]/index.js
+++ pages/fact/[id]/index.js
@@ -58,6 +58,7 @@ const Fact = ({ fact, totalLength }) => {
             <button
               type="button"
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              onClick={() => setShowModal(false)}
             >
               <svg
                 aria-hidden="true"
