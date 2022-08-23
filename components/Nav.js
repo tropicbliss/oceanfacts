80147d23584a07fe7843c06a17fa7c74dd321a9b
tropicbliss <tropicbliss@protonmail.com>
2022-08-23 17:04:39 +0800

    E

--- components/Nav.js
+++ components/Nav.js
@@ -58,18 +58,18 @@ const Nav = () => {
                 API
               </li>
             </Link>
-            <li
-              onClick={() => setMenuExpanded(false)}
-              className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+            <a
+              href="https://github.com/tropicbliss/oceanfacts"
+              target="_blank"
+              rel="noreferrer"
             >
-              <a
-                href="https://github.com/tropicbliss/oceanfacts"
-                target="_blank"
-                rel="noreferrer"
+              <li
+                onClick={() => setMenuExpanded(false)}
+                className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 GitHub
-              </a>
-            </li>
+              </li>
+            </a>
           </ul>
         </div>
       </div>
